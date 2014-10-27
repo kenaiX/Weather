098daf73bad213429f274171e1f005c580897de7
余俊卿 <yujunqing@meizu.com>
2014-10-27 12:58:39 +0800

    test weather api

---
+++ weathertest/src/main/java/ApiTest.java
@@ -0,0 +1,32 @@
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class ApiTest {
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        client.setConnectTimeout(3, TimeUnit.SECONDS);
+
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/rest?city=010600")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.print(response.body().string());
+
+
+
+        request = new Request.Builder()
+                .url("http://localhost:8080/rest?city=010601")
+                .build();
+
+        response = client.newCall(request).execute();
+
+        System.out.print(response.body().string());
+    }
+}
